4e185e3caf7c7ff203e61d43b51feac7055f0204
felipe <felpmel@gmail.com>
2019-10-09 23:50:16 -0300

    primeiro

---
+++ Assets/PlayerMoviment.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    public Rigidbody rb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(0,-200,-500);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
