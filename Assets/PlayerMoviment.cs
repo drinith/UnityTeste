c480ee5ebe79a5c04a810e6ac2a57de9447d9364
felipe <felpmel@gmail.com>
2019-10-10 18:51:35 -0300

    Parte 1 2 3

--- Assets/PlayerMoviment.cs
+++ Assets/PlayerMoviment.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class PlayerMoviment : MonoBehaviour
 {
     public Rigidbody rb;
+
+    public float forwardForce = 200f;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.AddForce(0,-200,-500);
+        
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
+        rb.AddForce(0,0,forwardForce*Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(500*Time.deltaTime,0,0);
+        }
+        if(Input.GetKey("a"))
+        {
+            rb.AddForce(-500*Time.deltaTime,0,0);
+        }
+        
     }
+
+
+    
 }
